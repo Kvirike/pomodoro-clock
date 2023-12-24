b893deda9169a8047fede60dd3d221a26081752d
Kvirike kemularia <kvirikekemularia@gmail.com>
2023-12-25 00:36:23 +0400

    session length
    setting up session length using useState hook and setting up visuals

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [displayTime, setDisplayTime]=useState(25*60)
+  const [displayTime, setDisplayTime]=useState(25*60);
+  const [breakTime, setBreakTime] = useState(5 * 60);
+  const [sessionTime, setSessionTime] = useState(25 * 60);
+  const [timerOn, setTimerOn] = useState(false);
+
 
   const formatTime = (time) => {
     let minutes = Math.floor(time / 60);
@@ -15,17 +19,61 @@ function App() {
     )
   }
 
+  const changeTime = (amount, type) =>{
+    if(type == 'break'){
+      if(breakTime <= 60 && amount < 0){
+        return;
+      }
+      setBreakTime((prev) => prev + amount);
+    }else{
+      if(sessionTime <= 60 && amount < 0){
+        return;
+      }
+      setSessionTime((prev) => prev + amount)
+      if(!timerOn){
+        setDisplayTime(sessionTime + amount)
+      }
+    }
+  }
+  const controlTime = () => {
+    
+  }
+  const resetTime = () => {
+    setDisplayTime(25 * 60);
+    setBreakTime(5 * 60);
+    setSessionTime(25 * 60);
+  }
 
   return (
-    <div>
-      <Length 
-        title={"break length"} 
-        changeTime={null} 
-        type={"break"} 
-        time={null} 
-        formatTime={formatTime}
+    <div className='center-align'>
+      <h1>Pomodoro Clock</h1>
+      <div className='dual-container'>
+        <Length 
+          title={"break length"} 
+          changeTime={changeTime} 
+          type={"break"} 
+          time={breakTime} 
+          formatTime={formatTime}
         />
+        <Length 
+          title={"session length"} 
+          changeTime={changeTime} 
+          type={"session"} 
+          time={sessionTime} 
+          formatTime={formatTime}
+        />
+      </div>
       <h1>{formatTime(displayTime)}</h1>
+      <button className='"btn-large deep-purple lighten-2' onClick={controlTime}>
+        {timerOn ? 
+          (<i className='material-icons'>pause_circle_filled</i>)
+          :
+          (<i className='material-icons'>play_circle_filled</i>)
+        }
+      </button>
+      <button className='"btn-large deep-purple lighten-2' onClick={resetTime}>
+        <i className='material-icons'>autorenew</i>
+      </button>
     </div>
   );
 }
@@ -35,11 +83,11 @@ function Length({title, changeTime, type, time, formatTime}){
     <div>
       <h3>{title}</h3>
       <div className='time-sets'>
-        <button className='btn-small deep-purpel lighten-2'>
+        <button className='btn-small deep-purpel lighten-2' onClick={() => changeTime(-60, type)}>
           <i className='material-icons'>arrow_downward</i>
         </button>
         <h3>{formatTime(time)}</h3>
-        <button className='btn-small deep-purpel lighten-2'>
+        <button className='btn-small deep-purpel lighten-2' onClick={() => changeTime(60, type)}>
           <i className='material-icons'>arrow_upward</i>
         </button>
       </div>
